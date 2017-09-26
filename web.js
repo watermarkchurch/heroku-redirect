21d1ad2c8377b46916ab03a8e3e589cc1d93505f
Gordon <gburgett@watermark.org>
2017-09-26 15:22:55 -0500

    make it redirect to theporch.live/live-stream always

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var port = process.env.PORT || 5000;
 
 app.get('*', function(request, response) {
-  response.redirect(newBaseURL + request.url);
+  response.redirect(301, 'http://www.theporch.live/live-stream');
 });
 
 app.listen(port, function() {
