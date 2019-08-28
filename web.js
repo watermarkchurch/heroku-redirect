379d2f3f29a9318e4889facd36e6e15ab54f4198
Gordon <gburgett@watermark.org>
2019-08-28 11:16:15 -0500

    Do proper URL redirection with URL lib

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var { URL } = require('url')
 var app = express();
 
 var port = process.env.PORT || 5000;
+var baseUrl = new URL(process.env.NEW_BASE_URL)
 
 app.get('*', function(request, response) {
-  response.redirect(301, 'http://www.theporch.live/live-stream');
+  var newUrl = new URL(request.url, baseUrl)
+  response.redirect(301, newUrl.toString());
 });
 
 app.listen(port, function() {
