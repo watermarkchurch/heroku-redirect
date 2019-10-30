294b9f65ba95f131928a456bd7aa06f1208ea145
Gordon <gburgett@watermark.org>
2019-10-30 15:24:38 -0500

    Allow doing rules with hostname match

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var { URL } = require('url')
+var { URL } = require('url');
+var pathToRegexp = require('path-to-regexp');
 var app = express();
 
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 301);
 var port = process.env.PORT || 5000;
 var baseUrl = new URL(process.env.NEW_BASE_URL)
 
-Object.keys(process.env).filter(k => /RULE_/.test(k)).forEach(ruleName => {
+var rules = Object.keys(process.env).filter(k => /RULE_/.test(k)).map(ruleName => {
   const rule = process.env[ruleName]
 
   const [pattern, to, status] = rule.split(' ').map((s) => s.trim())
@@ -15,18 +16,30 @@ Object.keys(process.env).filter(k => /RULE_/.test(k)).forEach(ruleName => {
     statusCode = parseInt(status)
   } catch(ex) {
   }
+  statusCode = statusCode || redirectStatus
 
-  app.get(pattern, function(request, response) {
-    var newUrl = new URL(to)
-    response.redirect(statusCode || 301, newUrl.toString());
-  })
+  return { pattern: pathToRegexp(pattern), to: new URL(to), status: statusCode, name: ruleName }
 })
 
 app.get('*', function(request, response) {
-  var newUrl = new URL(request.url, baseUrl)
-  response.redirect(redirectStatus, newUrl.toString());
+  var requestUrl = new URL(`${request.protocol}://${request.host}${request.path}`)
+
+  requestUrl = requestUrl.toString()
+  var rule = rules.find((r) => r.pattern.test(requestUrl) || r.pattern.test(request.path))
+  if (rule) {
+    var newUrl = new URL(rule.to).toString()
+    console.log('Match', rule.name, rule.status, requestUrl, '=>', newUrl)
+    response.redirect(rule.status, newUrl);
+    return
+  }
+  
+
+  var newUrl = new URL(request.url, baseUrl).toString()
+  console.log('No Match:', redirectStatus, requestUrl, '=>', newUrl)
+  response.redirect(redirectStatus, newUrl);
 });
 
 app.listen(port, function() {
   console.log("Listening on " + port);
+  process.send('listening')
 });
---
+++ web.test.ts
@@ -0,0 +1,93 @@
+import { fork, ChildProcess } from 'child_process'
+import fetch from 'node-fetch'
+
+describe('web', () => {
+  let child: ChildProcess = null
+
+  afterEach((done) => {
+    child.kill()
+    child.on('exit', function() {
+      done()
+    })
+  })
+
+  it('redirects to base URL', async () => {
+    await forkIt()
+
+    const resp = await fetch('http://localhost:5000/test', {
+      redirect: 'manual'
+    })
+
+    expect(resp.status).toEqual(301)
+    expect(resp.headers.get('Location')).toEqual('https://www.watermark.org/test')
+  })
+
+  it('redirects to base URL with status code', async () => {
+    await forkIt({
+      REDIRECT_STATUS: '302'
+    })
+
+    const resp = await fetch('http://localhost:5000/test', {
+      redirect: 'manual'
+    })
+
+    expect(resp.status).toEqual(302)
+    expect(resp.headers.get('Location')).toEqual('https://www.watermark.org/test')
+  })
+
+  it('redirects to matched URL with status', async () => {
+    await forkIt({
+      REDIRECT_STATUS: '302',
+      RULE_1: '* https://www.watermark.org/frisco 301'
+    })
+
+    const resp = await fetch('http://localhost:5000/test', {
+      redirect: 'manual'
+    })
+
+    expect(resp.status).toEqual(301)
+    expect(resp.headers.get('Location')).toEqual('https://www.watermark.org/frisco')
+  })
+
+  it('redirects match by hostname', async () => {
+    await forkIt({
+      RULE_1: 'https?://wmfw.org/* https://www.watermark.org/fort-worth 302',
+      RULE_2: 'https?://www.watermarkfrisco.org/* https://www.watermark.org/frisco',
+    })
+
+    const resp = await fetch('http://localhost:5000/test', {
+      headers: {
+        host: 'wmfw.org'
+      },
+      redirect: 'manual'
+    })
+
+    expect(resp.headers.get('Location')).toEqual('https://www.watermark.org/fort-worth')
+    expect(resp.status).toEqual(302)
+
+    const resp2 = await fetch('http://localhost:5000', {
+      headers: {
+        host: 'www.watermarkfrisco.org'
+      },
+      redirect: 'manual'
+    })
+
+    expect(resp2.headers.get('Location')).toEqual('https://www.watermark.org/frisco')
+    expect(resp2.status).toEqual(301)
+  })
+
+  function forkIt(env?: { [key: string]: string }): Promise<void> {
+    child = fork('./web', [], {
+      env: {
+        NEW_BASE_URL: 'https://www.watermark.org',
+        ...env
+      }
+    })
+
+    return new Promise((resolve) => {
+      child.once('message', () => {
+        resolve()
+      })
+    })
+  }
+})
